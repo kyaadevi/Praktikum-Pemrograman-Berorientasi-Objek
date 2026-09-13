7addeae55459f23d25cb80c6a304dbf22acaea29
kyadevi <kyadevi22@gmail.com>
2026-09-13 16:33:21 +0700

    Add files via upload

---
+++ kyadevi_project1/Kyadevi_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kyadevi_project1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Kyadevi_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Rizkia Devi Nur Chairinnisa");
+        System.out.println("NPM: 2505060033");
+        System.out.println("Alamat: Secang, Magelang");
+        System.out.println("No_HP: 08123456789");
+    }
+}
